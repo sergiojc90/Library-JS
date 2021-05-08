4f4ccf3080fd90e7ff3e6c915d0cf556b2a9a287
sergiojc90 <sergiojc90@gmail.com>
2021-05-07 22:21:03 -0500

    Creating the main JS file for the library project

---
+++ library.js
@@ -0,0 +1,5 @@
+// First, we are going to create a constructor for making "Book" objects.
+// The "Book" object is going to have the following attributes:
+// Title, author, number of pages and whether or not you have read the book.
+
+
