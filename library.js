818da9a4f3aae8e57a75a93300315155144514bf
sergiojc90 <sergiojc90@gmail.com>
2021-05-07 22:28:25 -0500

    Creating the constructor for the Book object

--- library.js
+++ library.js
@@ -2,4 +2,10 @@
 // The "Book" object is going to have the following attributes:
 // Title, author, number of pages and whether or not you have read the book.
 
-
+    function Book(title, author, pages, status){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+        this.info = title + " by" + author + ", " + pages + " pages, " + status;
+    }
